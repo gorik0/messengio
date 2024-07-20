2a73abeb0802b80b144d4a01e1b0be803a610467
gorik0 <delavarus@gmail.com>
2024-07-20 14:10:18 +0300

    sql_store

--- backend/store.go
+++ backend/store.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"database/sql"
+)
+
 type Store interface {
 	GetMessages() ([]string, error)
 	PushMessage(msg string) error
@@ -23,4 +27,46 @@ var _ Store = &MemoryStore{}
 
 func NewStore() Store {
 	return &MemoryStore{}
+}
+
+type SqlStore struct {
+	db *sql.DB
+}
+
+func (s SqlStore) GetMessages() ([]string, error) {
+	q := "SELECT message FROM messages  "
+	query, err := s.db.Query(q, nil)
+	var msgs []string
+	if err != nil {
+		return msgs, err
+	}
+
+	for query.Next() {
+		var msg string
+		err := query.Scan(&msg)
+		if err != nil {
+			return nil, err
+		}
+		msgs = append(msgs, msg)
+	}
+	return msgs, nil
+
+}
+
+func (s SqlStore) PushMessage(msg string) (err error) {
+	q := "INSERT INTO messages (message) VALUES (?)"
+	_, err = s.db.Exec(q, msg)
+	return err
+}
+
+var _ Store = &SqlStore{}
+
+func NewSqlStore(db *sql.DB) (Store, error) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS messages (message TEXT)")
+	if err != nil {
+		return nil, err
+	}
+
+	return &SqlStore{db: db}, nil
+
 }
